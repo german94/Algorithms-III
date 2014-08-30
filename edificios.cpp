a98f0faafd266dbba4e54d143212fd0174d52c2f
angelmore <angel_21_fer@hotmail.com>
2014-08-30 05:15:51 -0300

    nuevoEdificio

--- edificios.cpp
+++ edificios.cpp
@@ -14,6 +14,10 @@ struct Edificio
 	{
 		return this->a_ < otra.a_;
 	}
+	bool operator!=(const Edificio& otra) const
+	{
+		return this->a_ != otra.a_ && this->i_ != otra.i_ && this->d_ != otra.d_;
+	}
 };
 
 vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cantidad
@@ -24,8 +28,7 @@ vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cant
 	int comparo = 0;
 	int siguiente = 1; 
 
-	res.push_back(v[comparo].i_);
-	res.push_back(v[comparo].a_);
+	res.push_back(v[comparo].i_); res.push_back(v[comparo].a_);
 	Edificio e_comparo = v[comparo];
 	Edificio e_siguiente = v[siguiente];
 	while(siguiente < total_de_edificios) 
@@ -34,20 +37,40 @@ vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cant
  			{
  				if(e_comparo < e_siguiente) // el siguiente edificio es mas a_o estrictamente
  				{	//entonces cambia la a_ura, es una solucion.  
- 					res.push_back(e_siguiente.i_);
-					res.push_back(e_siguiente.a_);
- 					heap.push(e_comparo);
+ 					res.push_back(e_siguiente.i_); res.push_back(e_siguiente.a_);
+ 					if(heap.empty())
+ 						{	
+ 							heap.push(e_comparo);
+ 						}
+ 						else
+ 						{
+ 							if (heap.top() != e_comparo) { heap.push(e_comparo);}
+ 						}	
  					comparo = siguiente;
- 					siguiente++;
  					e_comparo = v[comparo];
- 					e_siguiente = v[siguiente];
  				}	
  				else
  				{ // el siguiente edifio es mas chico lo sa_eo momentaneamente
- 					heap.push(e_siguiente);
- 					siguiente++;
- 					e_siguiente = v[siguiente]; // ahora comparo con el siguiente al siguiente, con el cual voy a comparar se mantiene
+ 					if(e_comparo.a_ == e_siguiente.a_)
+ 					{
+ 						if(heap.empty())
+ 						{	
+ 							heap.push(e_comparo);
+ 						}
+ 						else
+ 						{
+ 							if (heap.top() != e_comparo) { heap.push(e_comparo);}
+ 						}	
+ 						comparo = siguiente;
+ 						e_comparo = v[comparo];
+ 					}
+ 					else
+ 					{	
+ 						heap.push(e_siguiente);
+ 					}
  				}
+ 				siguiente++;
+ 				e_siguiente = v[siguiente]; 
  			}
 			else // el siguiente edificio no me corta, entonces puede que en el medio de estos dos
  	//	(comparar y siguiente) haya algun techo que quedo de antes es decir, el contorno
@@ -57,8 +80,7 @@ vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cant
  				{
  					if(heap.top().d_ > e_comparo.d_) 
  					{ // luego del comparo, la a_ura disminuye hasta la del proximo, esta es una solucion
- 						res.push_back(e_comparo.d_);
-						res.push_back(heap.top().a_);
+ 						res.push_back(e_comparo.d_); res.push_back(heap.top().a_);
  					 	e_comparo = heap.top(); //ahora voy a comparar el que estaba en el heap con siguiente
  						break;
  					}	// else si el que estaba proximo su lado d_echo es menor que el de comparo
@@ -66,13 +88,7 @@ vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cant
  				}
  				if (heap.empty()) //si sali del heap porque me quede sin edificios
  				{// entonces no hay nada entre ambos edificios la a_ura del siguiente es solucion
- 			//ya que termina el v[comparo], luego la a_ura es la del piso y asciendo con
- 			// la a_ura del edificio siguiente
- 					
- 					res.push_back(e_comparo.d_);
-					res.push_back(0);
-					res.push_back(e_siguiente.i_);
-					res.push_back(e_siguiente.a_);
+ 					res.push_back(e_comparo.d_); res.push_back(0); res.push_back(e_siguiente.i_); res.push_back(e_siguiente.a_);
  					comparo = siguiente; // no encolo nada porque es como si el ejercicio empezara de nuevo
  					siguiente++; //  
  					e_comparo = v[comparo];
@@ -80,25 +96,23 @@ vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cant
  				} // si el heap no esta vacio entonces el if de adentro del for ya dejo arreglado
  			}// quien es el comparar y siguiente;
  	}//comparo quedo dentro del limite 
- 	if (!heap.empty()) //ESTO ES NUEVO, ES EL CASO EN EL QUE SALIA DEL CICLO PORQUE 
- 		//YA NO TENIA UN SIGUIENTE EDIFICIO, PERO.. ME QUEDABAN ALGUNOS EN EL HEAP
- 		// POR ANALIZAR
+ 	if (!heap.empty()) //por si ya no hay sgtes pero si me quedaron algunos en el heap
  	{
- 		while(!heap.empty())
+ 		for (;!heap.empty(); heap.pop())
  		{
  			if(heap.top().d_ > e_comparo.d_) 
  				{ // luego del comparo, la a_ura disminuye hasta la del proximo, esta es una solucion
- 					res.push_back(e_comparo.d_);
-					res.push_back(heap.top().a_);
- 				 	e_comparo = heap.top(); //ahora voy a comparar el que estaba en el heap con siguiente
- 				}	// else si el que estaba proximo su lado d_echo es menor que el de comparo
- 			//significa que quedo atras el del heap entonces lo desencolo y me olvido de ese
- 		heap.pop();
+ 					if (heap.top() < e_comparo)
+ 					{	
+ 						res.push_back(e_comparo.d_);
+						res.push_back(heap.top().a_);
+					}
+					e_comparo = heap.top();	// else si el que estaba proximo su lado d_echo es menor que el de comparo			
+ 				} 	//significa que quedo atras el del heap entonces lo desencolo y me olvido de ese
  		}
  	}
  	res.push_back(e_comparo.d_);
 	res.push_back(0);
-
 	return res;
  }
 
@@ -106,8 +120,8 @@ vector<int> edificios(vector<Edificio> v, int total_de_edificios) //n es la cant
 int main()
 {
 	
-	int a[] = {2, 8, 20, 3, 12, 5, 7, 9, 11, 13, 10, 19};
-	int tam = 12;
+	int a[] = {1, 2, 10, 2, 3, 11, 3, 4, 5, 4, 5, 6, 6, 5, 7, 8, 6, 9};
+	int tam = 18;
 	int cant= tam/3;
 	vector<Edificio> entrada;
 
